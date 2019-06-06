25824169f5cf4eb9a7bc2c5eed6f783b4da1a69b
siva0323 <47570561+siva0323@users.noreply.github.com>
2019-06-06 19:28:23 +0530

    commit 2.0
    commit

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 class hellojava{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java"); 
+        System.out.println("Hello Java");
     }  
 }  
